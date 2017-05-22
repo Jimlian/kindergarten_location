69d06f1975d8433ec9b8a5a519b52605cc8242e2
Lianxu <lianxu@LianxudeMacBook-Pro.local>
2017-05-22 13:28:30 +0800

    change job to kindergarten

--- app/controllers/kindergartens_controller.rb
+++ app/controllers/kindergartens_controller.rb
@@ -24,9 +24,9 @@ class KindergartensController < ApplicationController
       @location_id = Location.find_by(name: @location)
 
       if @location == '所有城市'
-        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.published.recent.paginate(:page => params[:page], :per_page => 10)
       else
-        @jobs = Job.where(:location => @location_id).published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.where(:location => @location_id).published.recent.paginate(:page => params[:page], :per_page => 10)
       end
 
     # 判断是否筛选职位类型 #
@@ -35,29 +35,29 @@ class KindergartensController < ApplicationController
       @category_id = Category.find_by(name: @category)
 
       if @category == '所有类型'
-        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.published.recent.paginate(:page => params[:page], :per_page => 10)
       else
-        @jobs = Job.where(:category => @category_id).published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.where(:category => @category_id).published.recent.paginate(:page => params[:page], :per_page => 10)
       end
 
     # 判断是否筛选薪水 #
     elsif params[:wage].present?
       @wage = params[:wage]
       if @wage == '5k以下'
-        @jobs = Job.wage1.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.wage1.published.recent.paginate(:page => params[:page], :per_page => 10)
       elsif @wage == '5~10k'
-        @jobs = Job.wage2.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.wage2.published.recent.paginate(:page => params[:page], :per_page => 10)
       elsif @wage == '10~15k'
-        @jobs = Job.wage3.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.wage3.published.recent.paginate(:page => params[:page], :per_page => 10)
       elsif @wage == '15~25k'
-        @jobs = Job.wage4.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.wage4.published.recent.paginate(:page => params[:page], :per_page => 10)
       else
-        @jobs = Job.wage5.published.recent.paginate(:page => params[:page], :per_page => 10)
+        @kindergartens = Kindergarten.wage5.published.recent.paginate(:page => params[:page], :per_page => 10)
       end
 
     # 预设显示所有公开职位 #
     else
-      # @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 1)
+      @kindergartens = Kindergarten.published.recent.paginate(:page => params[:page], :per_page => 1)
     end
   end
 
@@ -152,7 +152,7 @@ class KindergartensController < ApplicationController
    private
 
    def kindergarten_params
-     params.require(:kindergarten).permit(:title, :description, :fee, :phone, :is_hidden, :location)
+     params.require(:kindergarten).permit(:title, :description, :fee, :phone, :is_hidden, :location, :location)
    end
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,9 +52,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "moli_#{Rails.env}"
+  # Use a real queuing backend for Active kindergarten (and separate queues per environment)
+  # config.active_kindergarten.queue_adapter     = :resque
+  # config.active_kindergarten.queue_name_prefix = "moli_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
